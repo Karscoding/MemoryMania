4eaa9d91308102c608729a9c3824d68fffa889a0
Koninglukaas <Lucasbruins@outlook.com>
2024-06-20 21:24:12 +0200

    Display 3 lists

--- angular-app/src/app/aggregate-data/aggregate-data.component.ts
+++ angular-app/src/app/aggregate-data/aggregate-data.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
-import { AgChartsAngular } from 'ag-charts-angular';
-import { AgChartOptions } from 'ag-charts-community';
 import { CommonModule } from '@angular/common';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
@@ -9,37 +7,16 @@ import { throwError } from 'rxjs';
 @Component({
   selector: 'app-aggregate-data',
   standalone: true,
-  imports: [AgChartsAngular, CommonModule],
+  imports: [CommonModule],
   templateUrl: './aggregate-data.component.html',
   styleUrls: ['./aggregate-data.component.css']
 })
 export class AggregateDataComponent implements OnInit {
-  aggregateData: any;
+  aggregateData: any[] = [];
   players: any[] = [];
   dates: any[] = [];
 
-  // Chart options
-  public gamesChartOptions: AgChartOptions;
-  public playersChartOptions: AgChartOptions;
-  public datesChartOptions: AgChartOptions;
-
-  constructor(private dataService: DataService) {
-    // Initialize chart options
-    this.gamesChartOptions = {
-      data: [],
-      series: [{ type: 'bar', xKey: 'label', yKey: 'value', yName: 'Total Games' }]
-    };
-
-    this.playersChartOptions = {
-      data: [],
-      series: [{ type: 'bar', xKey: 'username', yKey: 'count', yName: 'Players' }]
-    };
-
-    this.datesChartOptions = {
-      data: [],
-      series: [{ type: 'bar', xKey: 'date', yKey: 'count', yName: 'Games per Day' }]
-    };
-  }
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.loadAggregateData();
@@ -55,10 +32,6 @@ export class AggregateDataComponent implements OnInit {
       })
     ).subscribe(data => {
       this.aggregateData = data;
-      this.gamesChartOptions = {
-        ...this.gamesChartOptions,
-        data: [{ label: 'Total Games', value: this.aggregateData.totalGames }]
-      };
     });
   }
 
@@ -70,30 +43,17 @@ export class AggregateDataComponent implements OnInit {
       })
     ).subscribe(data => {
       this.players = data;
-      this.playersChartOptions = {
-        ...this.playersChartOptions,
-        data: this.players.map(player => ({ username: player.username, count: 1 }))
-      };
     });
   }
 
   loadDates() {
-    this.dataService.getDates().subscribe(
-      data => {
-        console.log('Dates response:', data);  // Log the response to check the format
-        if (Array.isArray(data)) {
-          this.dates = data;
-          this.datesChartOptions = {
-            ...this.datesChartOptions,
-            data: this.dates.map(date => ({ date: date.date, count: date.count }))
-          };
-        } else {
-          console.error('Expected an array but received:', data);
-        }
-      },
-      error => {
+    this.dataService.getDates().pipe(
+      catchError(error => {
         console.error('Error fetching dates:', error);
-      }
-    );
+        return throwError(error);
+      })
+    ).subscribe(data => {
+      this.dates = Object.keys(data).map(key => ({ date: key, count: data[key] }));
+    });
   }
 }
