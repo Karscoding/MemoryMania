ab6a663cf9544284e7d228c4370c49fd7e872450
Koninglukaas <Lucasbruins@outlook.com>
2024-06-20 15:43:30 +0200

    Data component en service
    Created dataservice to handle API calls for admin endpoints.
    
    Created aggregatedatacomponent to display aggregated data on admin page

--- angular-app/src/app/aggregate-data/aggregate-data.component.ts
+++ angular-app/src/app/aggregate-data/aggregate-data.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../services/data.service";
 
 @Component({
   selector: 'app-aggregate-data',
@@ -7,6 +8,35 @@ import { Component } from '@angular/core';
   templateUrl: './aggregate-data.component.html',
   styleUrl: './aggregate-data.component.css'
 })
-export class AggregateDataComponent {
+export class AggregateDataComponent implements OnInit {
+  aggregateData: any;
+  players: any[] = [];
+  dates: any[] = [];
 
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit(): void {
+    this.loadAggregateData();
+    this.loadPlayers();
+    this.loadDates();
+  }
+
+  loadAggregateData() {
+    this.dataService.getAggregateData().subscribe(data => {
+      this.aggregateData = data;
+    });
+  }
+
+  loadPlayers() {
+    this.dataService.getPlayers().subscribe(data => {
+      this.players = data;
+    });
+  }
+
+  loadDates() {
+    this.dataService.getDates().subscribe(data => {
+      this.dates = data;
+    });
+  }
 }
--- angular-app/src/app/services/data.service.ts
+++ angular-app/src/app/services/data.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private apiUrl = 'http://localhost:8000/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getHeaders() {
+    return new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`);
+  }
+
+  getAggregateData() {
+    return this.http.get<any>(`${this.apiUrl}/api/admin/aggregate`, {headers: this.getHeaders()});
+  }
+
+  getPlayers() {
+    return this.http.get<any>(`${this.apiUrl}/api/admin/players`, {headers: this.getHeaders()});
+  }
+
+  getDates() {
+    return this.http.get<any>(`${this.apiUrl}/api/admin/dates`, {headers: this.getHeaders()});
+  }
 }
