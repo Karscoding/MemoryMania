522cde624d903cb963d9e0a1a398bd1a4a69b2e4
Koninglukaas <Lucasbruins@outlook.com>
2024-06-20 15:00:50 +0200

    Routes added

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {AggregateDataComponent} from "./aggregate-data/aggregate-data.component";
+import {LoginComponent} from "./login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: AggregateDataComponent},
+  {path: 'login', component: LoginComponent}
+];
